5d0403849fe1e5ec0ed6b441a0a18127f2807de6
Yohan <yohanbeaumatin@gmail.com>
2025-12-17 19:28:58 +0100

    UP my solution

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -24,6 +24,7 @@ from sklearn.metrics.pairwise import pairwise_distances
 
 class KNearestNeighbors(ClassifierMixin, BaseEstimator):
     """KNearestNeighbors classifier."""
+
     def __init__(self, n_neighbors=1):  # noqa: D107
         self.n_neighbors = n_neighbors
 
@@ -119,6 +120,7 @@ class MonthlySplit(BaseCrossValidator):
         for which this column is not a datetime, it will raise a ValueError.
         To use the index as column just set `time_col` to `'index'`.
     """
+
     def __init__(self, time_col='index'):  # noqa: D107
         self.time_col = time_col
 
